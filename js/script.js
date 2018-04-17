7191f9e439c5ac4b6136303ecb5f2d449aeb6e48
Krystian Bieniek <krystekk2013@gmail.com>
2018-04-17 14:23:02 +0200

    Add zadanie_8_5

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter a');
+	h = prompt('Enter h');
+	triangleArea = a*h/2
+alert('triangleArea') /* Dlaczego nie wyświetla wyniku w komunikacie?*/
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
